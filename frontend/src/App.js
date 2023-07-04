b99018e02ad1552baca6ff7eb81a330d67a58700
Krishna-7777 <kchhabraji13@gmail.com>
2023-07-04 13:28:06 +0530

    Settings Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Settings from './components/Settings/Settings';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Settings />
     </div>
   );
 }
